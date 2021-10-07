f6b8085707ac5cc52ec760c89483b5130a77d4b6
Henry Miller Maia Bilby <hmmb-eng@outlook.com>
2021-10-07 10:27:50 -0400

    Inicio do Projeto

---
+++ Minha_HQ_Preferida/Minha_HQ_Preferida/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Minha_HQ_Preferida
+//
+//  Created by Henry Bilby on 07/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
