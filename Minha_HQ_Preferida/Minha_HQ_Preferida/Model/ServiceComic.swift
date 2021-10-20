725617dbca74f9d52a643a514c5dff7ab216473a
Sara Felix <sara.felix.ext@sodexo.com>
2021-10-20 00:01:35 -0300

    implementando collection view

---
+++ Minha_HQ_Preferida/Minha_HQ_Preferida/Model/ServiceComic.swift
@@ -0,0 +1,32 @@
+//
+//  ServiceComic.swift
+//  Minha_HQ_Preferida
+//
+//  Created by Sara on 17/10/21.
+//
+
+import UIKit
+
+class ServiceComic {
+    
+    public func getComicList(completion: ([ComicElement]?, String)->Void) {
+        
+        let comicList : [ComicElement]?
+        
+        do {
+            comicList = [
+                .init(number: "#70", title: "O Espetacular Homem Aranha(2018)", description: "", thumbnail: Thumbnail(caminho: "setenta", extensao: "jpg"), selected: false),
+                .init(number: "#36", title: "O Espetacular Homem-Aranha(2020)", description: "", thumbnail: Thumbnail(caminho: "trintaeseis", extensao: "jpg"), selected: false),
+                .init(number: "#1", title: "O Espetacular Homem-Aranha(2019)", description: "", thumbnail: Thumbnail(caminho: "um", extensao: "jpg"), selected: false),
+                .init(number: "#301", title: "O Espetacular Homem-Aranha(2017)", description: "", thumbnail: Thumbnail(caminho: "trezentoseum", extensao: "jpg"), selected: false),
+                .init(number: "#60", title: "O Incrível Homem-Aranha(2018)", description: "", thumbnail: Thumbnail(caminho: "sessenta", extensao: "jpg"), selected: false),
+                .init(number: "#4", title: "O Espetacular Homem-Aranha (2017)", description: "", thumbnail: Thumbnail(caminho: "quatro", extensao: "jpg"), selected: false),
+            ]
+            
+            completion(comicList, "Sucesso ao carregar os personagens.")
+        } catch let error {
+            print("Erro ao carregar os personagens: \(error)")
+            completion(nil, "Erro ao carregar os personagens.")
+        }
+    }
+}
---
+++ Minha_HQ_Preferida/Minha_HQ_Preferida/View/ComicCollectionViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ComicCollectionViewCell.swift
+//  Minha_HQ_Preferida
+//
+//  Created by Sara on 17/10/21.
+//
+
+import UIKit
+
+class ComicCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleComic: UILabel!
+    @IBOutlet weak var imageComic : UIImageView!
+    
+    public func setup(with selectedComic: ComicElement) {
+        imageComic.image = UIImage(named: selectedComic.image)
+        titleComic.text = selectedComic.title
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        imageComic.layer.cornerRadius = 20
+    }
+    
+}
