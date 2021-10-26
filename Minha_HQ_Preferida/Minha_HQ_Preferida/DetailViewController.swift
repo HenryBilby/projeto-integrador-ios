6a85d8ac59bbacd4d38eb53d68409f9016863cef
Sara Felix <sarabfelix1@gmail.com>
2021-10-26 19:15:03 -0300

    inicio da sincronização

---
+++ Minha_HQ_Preferida/Minha_HQ_Preferida/DetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DetailViewController.swift
+//  Minha_HQ_Preferida
+//
+//  Created by Sara on 22/10/21.
+//
+
+import UIKit
+
+class DetailViewController {
+    
+    @IBOutlet weak var imageDetail: UIImageView!
+    
+    @IBOutlet weak var nameComic: UILabel!
+    
+    @IBOutlet weak var datePublished: UILabel!
+    
+    @IBOutlet weak var writerComic: UILabel!
+    
+    @IBOutlet weak var designComic: UILabel!
+    
+    @IBOutlet weak var frontCoverComic: UILabel!
+    
+    @IBOutlet weak var description: UILabel!
+    
+    
+}
